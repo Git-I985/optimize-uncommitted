6f876e442756955aa254db5111350fd78f924846
Edward Konovalov <edw4rd.young@gmail.com>
2023-10-24 01:59:57 +0300

    init

---
+++ optimize-uncommitted.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ -z $TNF_API_KEY ]]
+then
+  echo 'Error: TNF_API_KEY variable not found, please get api key from https://tinypng.com/developers and paste it in your ~/.zshrc or ~/.bashrc'
+  exit 1
+fi
+
+for file in $(git status -s | grep -E '^[^D]{2}' | cut -c4- | grep -E "(.png|.jpg|.webp|.jpeg)")
+do
+  response=$(curl -sS  https://api.tinify.com/shrink \
+     --user api:$TNF_API_KEY \
+     --data-binary @$file)
+
+  output_url=$(echo $response | grep -oE "http[^\"]+")
+
+  if [[ -z $output_url ]]
+  then
+    echo "Cant find output url for $file";
+    echo $response;
+    exit 1
+  fi
+
+  echo $file $output_url;
+
+  curl -sS $output_url \
+    --user api:$TNF_API_KEY \
+    --output $file;
+done;
